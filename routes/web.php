eff86c5053aca1e8bd067549378d0b0d2a0668f5
Asnur Ramdani <asnurramdhani12@gmail.com>
2021-11-19 20:55:21 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\SocialiteController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/auth/redirect', [SocialiteController::class, 'redirectToProvider'])->name('login-google');
+Route::get('/auth/callback', [SocialiteController::class, 'handleProviderCallback']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
